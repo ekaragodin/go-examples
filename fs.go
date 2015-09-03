2e89a5ea4b03413940efdcac5ef007066b788331
Eugeny Karagodin <ekaragodin@gmail.com>
2015-09-03 22:19:26 +0600

    Navigation by dirs

--- fs.go
+++ fs.go
@@ -9,6 +9,11 @@ import (
 
 var root = "/";
 
+type Entry struct {
+  Name string
+  FullName string
+}
+
 func main() {
   http.HandleFunc("/", indexHandler)
   log.Println("Server is started...")
@@ -16,12 +21,25 @@ func main() {
 }
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
+  path := r.URL.Query().Get("path")
+  if (path == "") {
+    path = root
+  }
+
   t, err := template.ParseFiles("templates/fs.html")
   if err != nil {
     log.Println(err.Error())
     http.Error(w, http.StatusText(500), 500)
   }
 
-  entries, _ := ioutil.ReadDir(root)
+  entries := []Entry{}
+  files, _ := ioutil.ReadDir(path)
+  for _, e := range files {
+    entries = append(entries, Entry{
+      Name: e.Name(),
+      FullName: path + "/" + e.Name(),
+    })
+  }
+
   t.Execute(w, entries)
 }
