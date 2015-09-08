8ac4678e9e39b3f89b77c079dfd6942873579425
Eugeny Karagodin <ekaragodin@gmail.com>
2015-09-08 22:07:56 +0600

    Add data directory

--- gallery.go
+++ gallery.go
@@ -4,14 +4,24 @@ import (
   "net/http"
   "html/template"
   "log"
+  "os"
+  "path"
 )
 
 var templates = template.Must(template.ParseFiles("templates/gallery.html"))
+var dataDir string
 
 func main() {
+  dataDir = getDataDir()
+
   http.HandleFunc("/", indexHandler)
   log.Println("Server is started...")
   http.ListenAndServe(":8000", nil)
+}
+
+func getDataDir() string {
+  pwd, _ := os.Getwd()
+  return path.Join(pwd, "data", "gallery")
 }
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
