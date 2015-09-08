ee0cbdec9d52206beddb4ce01470ee3c54e9e0a9
Eugeny Karagodin <ekaragodin@gmail.com>
2015-09-08 21:41:00 +0600

    Create gallery stub

---
+++ gallery.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+  "net/http"
+  "log"
+)
+
+func main() {
+  http.HandleFunc("/", indexHandler)
+  log.Println("Server is started...")
+  http.ListenAndServe(":8000", nil)
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+  println("hello!")
+}
